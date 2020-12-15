296899ba6ce43972a1d512a4ed1a76787c5cacd7
pragun <pragun.goyal@gmail.com>
2020-12-15 15:29:30 -0500

    Reduces the config entries to the ones that will be most likely used

--- user_config.h
+++ user_config.h
@@ -12,75 +12,24 @@
 
 auto array_of_config_key_values = array_of<User_Config_TypeDef>(
 	User_Config_TypeDef{
-		.application_id = 0, .keypad_key = 2,
-		.key_down = CFG(NO_REPORT,{}),
-		.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = SMALL_MOD_DIVISOR, .y_divisor = SMALL_MOD_DIVISOR, .z_divisor = 1,
-			.x_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_LEFT,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_RIGHT,0,0} })},
-			.y_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_DOWN,0,0} })},
-			.z_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_DOWN,0,0} })}, }),
-		.short_release = CFG(NO_REPORT,{}),
-		.long_release = CFG(NO_REPORT,{}),
-	}
-
-	, User_Config_TypeDef{
-	.application_id = 0, .keypad_key = 5,
-	.key_down = CFG(NO_REPORT,{}),
-	.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = SMALL_MOD_DIVISOR, .y_divisor = SMALL_MOD_DIVISOR, .z_divisor = 1,
-			.x_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RSHIFT, .keys = {KEY_LEFT,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RSHIFT, .keys = {KEY_RIGHT,0,0} })},
-			.y_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RSHIFT, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RSHIFT, .keys = {KEY_DOWN,0,0} })},
-			.z_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_DOWN,0,0} })}, }),
-	.short_release = CFG(NO_REPORT,{}),
-	.long_release = CFG(NO_REPORT,{}),
-	}
-
-	, User_Config_TypeDef{
-	.application_id = 0, .keypad_key = 8,
+	.application_id = 0, .keypad_key = 3,
 	.key_down = CFG(NO_REPORT,{}),
 	.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = BIG_MOD_DIVISOR, .y_divisor = BIG_MOD_DIVISOR, .z_divisor = 1,
 			.x_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_C,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = 0, .keys = {KEY_ENTER,0,0} })},
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_X,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_C,0,0} })},
 			.y_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_V,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_V,0,0} })},
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_V,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_V,0,0} })},
 			.z_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = 0, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = 0, .keys = {KEY_DOWN,0,0} })}, }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_Z,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_Y,0,0} })}, }),
 	.short_release = CFG(NO_REPORT,{}),
 	.long_release = CFG(NO_REPORT,{}),
 	}
 
-	, User_Config_TypeDef{
-	.application_id = 0, .keypad_key = 11,
-	.key_down = CFG(NO_REPORT,{}),
-	.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = BIG_MOD_DIVISOR, .y_divisor = BIG_MOD_DIVISOR, .z_divisor = 1,
-			.x_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_LEFT,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_RIGHT,0,0} })},
-			.y_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_DOWN,0,0} })},
-			.z_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} })}, }),
-	.short_release = CFG(NO_REPORT,{}),
-	.long_release = CFG(NO_REPORT,{}),
-	}
-
-	, User_Config_TypeDef{
-	.application_id = 0, .keypad_key = 1,
+	,User_Config_TypeDef{
+	.application_id = 0, .keypad_key = 6,
 	.key_down = CFG(NO_REPORT,{}),
 	.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = BIG_MOD_DIVISOR, .y_divisor = BIG_MOD_DIVISOR, .z_divisor = 1,
 			.x_movement_keys = {
@@ -96,19 +45,53 @@ auto array_of_config_key_values = array_of<User_Config_TypeDef>(
 	.long_release = CFG(NO_REPORT,{}),
 	}
 
+	,User_Config_TypeDef{
+		.application_id = 0, .keypad_key = 2,
+		.key_down = CFG(NO_REPORT,{}),
+		.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = SMALL_MOD_DIVISOR, .y_divisor = SMALL_MOD_DIVISOR, .z_divisor = 1,
+			.x_movement_keys = {
+			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_LEFT,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_RIGHT,0,0} })},
+			.y_movement_keys = {
+			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_UP,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_DOWN,0,0} })},
+			.z_movement_keys = {
+			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_ENTER,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = 0x00, .keys = {KEY_ESC,0,0} })}, }),
+		.short_release = CFG(NO_REPORT,{}),
+		.long_release = CFG(NO_REPORT,{}),
+	}
+
+	, User_Config_TypeDef{
+	.application_id = 0, .keypad_key = 5,
+	.key_down = CFG(NO_REPORT,{}),
+	.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = BIG_MOD_DIVISOR, .y_divisor = BIG_MOD_DIVISOR, .z_divisor = 1,
+			.x_movement_keys = {
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_TAB,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT | KEY_MOD_RSHIFT, .keys = {KEY_TAB,0,0} })},
+			.y_movement_keys = {
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_TAB,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL | KEY_MOD_RSHIFT, .keys = {KEY_TAB,0,0} })},
+			.z_movement_keys = {
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} })}, }),
+	.short_release = CFG(NO_REPORT,{}),
+	.long_release = CFG(NO_REPORT,{}),
+	}
+
 	, User_Config_TypeDef{
 	.application_id = 0, .keypad_key = 4,
 	.key_down = CFG(NO_REPORT,{}),
 	.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = BIG_MOD_DIVISOR, .y_divisor = BIG_MOD_DIVISOR, .z_divisor = 1,
 			.x_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL | KEY_MOD_RSHIFT, .keys = {KEY_LEFT,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL | KEY_MOD_RSHIFT, .keys = {KEY_RIGHT,0,0} })},
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_LEFT,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_RIGHT,0,0} })},
 			.y_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL | KEY_MOD_RSHIFT, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL | KEY_MOD_RSHIFT, .keys = {KEY_DOWN,0,0} })},
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_UP,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_DOWN,0,0} })},
 			.z_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_UP,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_DOWN,0,0} })}, }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} }),
+			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} })}, }),
 	.short_release = CFG(NO_REPORT,{}),
 	.long_release = CFG(NO_REPORT,{}),
 	}
@@ -131,21 +114,53 @@ auto array_of_config_key_values = array_of<User_Config_TypeDef>(
 	}
 
 	, User_Config_TypeDef{
-	.application_id = 0, .keypad_key = 10,
-	.key_down = CFG(NO_REPORT,{}),
-	.mouse_movement = CFG(MOTION_MOD_KEY_PRESS_RELEASE, {.x_divisor = BIG_MOD_DIVISOR, .y_divisor = BIG_MOD_DIVISOR, .z_divisor = 1,
-			.x_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT, .keys = {KEY_TAB,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RALT | KEY_MOD_RSHIFT, .keys = {KEY_TAB,0,0} })},
-			.y_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL, .keys = {KEY_TAB,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RCTRL | KEY_MOD_RSHIFT, .keys = {KEY_TAB,0,0} })},
-			.z_movement_keys = {
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} }),
-			KBRD_PRS_RLS({.modifier_keys = KEY_MOD_RMETA, .keys = {KEY_TAB,0,0} })}, }),
-	.short_release = CFG(NO_REPORT,{}),
-	.long_release = CFG(NO_REPORT,{}),
+		.application_id = 0, .keypad_key = 1,
+		.key_down = CFG(NO_REPORT,{}),
+		.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,{.x_factor = 2, .y_factor = 2, .z_factor = 2, }),
+		.short_release = CFG(NO_REPORT,{}),
+		.long_release = CFG(NO_REPORT,{}),
 	}
+
+	, User_Config_TypeDef{
+		.application_id = 0, .keypad_key = 8,
+		.key_down = CFG(NO_REPORT,{}),
+		.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,{.x_factor = 1, .y_factor = 1, .z_factor = 1, }),
+		.short_release = CFG(NO_REPORT,{}),
+		.long_release = CFG(NO_REPORT,{}),
+	}
+
+	, User_Config_TypeDef{
+		.application_id = 0, .keypad_key = 9,
+		.key_down = CFG(NO_REPORT,{}),
+		.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,{.x_factor = 1, .y_factor = 1, .z_factor = 1, }),
+		.short_release = CFG(NO_REPORT,{}),
+		.long_release = CFG(NO_REPORT,{}),
+	}
+
+	, User_Config_TypeDef{
+		.application_id = 0, .keypad_key = 10,
+		.key_down = CFG(NO_REPORT,{}),
+		.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,{.x_factor = 1, .y_factor = 1, .z_factor = 1, }),
+		.short_release = CFG(NO_REPORT,{}),
+		.long_release = CFG(NO_REPORT,{}),
+	}
+
+	, User_Config_TypeDef{
+		.application_id = 0, .keypad_key = 11,
+		.key_down = CFG(NO_REPORT,{}),
+		.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,{.x_factor = 1, .y_factor = 1, .z_factor = 1, }),
+		.short_release = CFG(NO_REPORT,{}),
+		.long_release = CFG(NO_REPORT,{}),
+	}
+
+	, User_Config_TypeDef{
+		.application_id = 0, .keypad_key = 12,
+		.key_down = CFG(NO_REPORT,{}),
+		.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,{.x_factor = 1, .y_factor = 1, .z_factor = 1, }),
+		.short_release = CFG(NO_REPORT,{}),
+		.long_release = CFG(NO_REPORT,{}),
+	}
+
 );
 
 
