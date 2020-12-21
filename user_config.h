176206682be45a12217d844c98c03b9dfad0d2e4
pragun <pragun.goyal@gmail.com>
2020-12-21 17:39:48 -0500

    Updates project to use reporting_functions.h from the new path

--- user_config.h
+++ user_config.h
@@ -10,6 +10,9 @@
 #define BIG_MOD_DIVISOR 40
 #define SMALL_MOD_DIVISOR 10
 
+#define FUSION_360_ID 1
+#define FUSION_360_NAME "fusion360.exe"
+
 auto array_of_config_key_values = array_of<User_Config_TypeDef>(
 	User_Config_TypeDef{
 		.application_id = 0, .keypad_key = 0,
@@ -181,34 +184,3 @@ auto array_of_config_key_values = array_of<User_Config_TypeDef>(
 
 std::array<Key_Size_Value_For_Flash, array_of_config_key_values.size()> config_ksv_objs;
 
-/*
-User_Config_TypeDef list_of_config_key_values[] =
-{
-		{
-				.application_id = 0,
-				.keypad_key = 0,
-				.key_down = CFG(NO_REPORT,{}),
-				.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,
-					{.x_factor = 1,
-					.y_factor = 1,
-					.z_factor = 1, }),
-				.short_release = CFG(NO_REPORT,{}),
-				.long_release = CFG(NO_REPORT,{}),
-		},
-
-			User_Config_TypeDef{
-			.application_id = 0, .keypad_key = 0, .key_down = CFG(NO_REPORT,{}),
-			.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT, {.x_factor = 1, .y_factor = 1, .z_factor = 1, }),
-			.short_release = CFG(NO_REPORT,{}),
-			.long_release = CFG(NO_REPORT,{}),
-	}
-
-	,User_Config_TypeDef{
-			.application_id = 0, .keypad_key = 1,
-			.key_down = CFG(KEYBOARD_PRESS_RELEASE, {.modifier_keys = 0x00, .keys = {0x04,0,0} }),
-			.mouse_movement = CFG(ALTERED_MOUSE_MOVEMENT,{.x_factor = 2, .y_factor = 2, .z_factor = 2, }),
-			.short_release = CFG(KEYBOARD_PRESS_RELEASE, {.modifier_keys = 0x02, .keys = {0x04,0,0} }),
-			.long_release = CFG(NO_REPORT,{}),
-	}
-};
-*/
